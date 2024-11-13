e4f1884ff075b7d3c77003d3aee6e773050f1bb3
Alexey Alter-Pesotskiy <alex@testableapple.com>
2024-11-13 15:57:13 +0000

    Update is_check_required action

--- lib/fastlane/plugin/stream_actions/actions/is_check_required.rb
+++ lib/fastlane/plugin/stream_actions/actions/is_check_required.rb
@@ -8,13 +8,19 @@ module Fastlane
 
         changed_files = Actions.sh("gh pr view #{params[:github_pr_num]} --json files -q '.files[].path'").split("\n")
 
-        return true if changed_files.size == 100 # TODO: https://github.com/cli/cli/issues/5368
+        too_many_files = changed_files.size > 99 # TODO: https://github.com/cli/cli/issues/5368
+        if too_many_files
+          UI.important("Check it required because there were too many files changed.")
+          return true
+        end
 
         changed_files.select! do |path|
           params[:sources].any? { |required| path.start_with?(required) }
         end
 
-        changed_files.size.positive?
+        is_check_required = changed_files.size.positive?
+        UI.important("Check is required: #{is_check_required}")
+        is_check_required
       end
 
       #####################################################
