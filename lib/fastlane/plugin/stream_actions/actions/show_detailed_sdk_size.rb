b22d7dddf5e0263da57775ca9122128e7995a228
Alexey Alter-Pesotskiy <alex@testableapple.com>
2026-01-19 13:33:15 +0000

    Fix lil issue

--- lib/fastlane/plugin/stream_actions/actions/show_detailed_sdk_size.rb
+++ lib/fastlane/plugin/stream_actions/actions/show_detailed_sdk_size.rb
@@ -3,8 +3,9 @@ module Fastlane
     class ShowDetailedSdkSizeAction < Action
       def self.run(params)
         chat_v5_branch = 'v5'
-        is_pr_to_chat_v5 = params[:github_repo].include?('stream-chat-swift') && ENV.fetch('GITHUB_BASE_REF', nil).to_s.include?(chat_v5_branch)
-        is_push_to_chat_v5 = ENV['GITHUB_EVENT_NAME'].to_s == 'push' && other_action.current_branch == chat_v5_branch
+        is_chat_repo = params[:github_repo].include?('stream-chat-swift')
+        is_pr_to_chat_v5 = is_chat_repo && ENV.fetch('GITHUB_BASE_REF', nil).to_s.include?(chat_v5_branch)
+        is_push_to_chat_v5 = is_chat_repo && ENV['GITHUB_EVENT_NAME'].to_s == 'push' && other_action.current_branch == chat_v5_branch
         UI.important("🏗️ Base branch workaround for Chat V5") if is_pr_to_chat_v5 || is_push_to_chat_v5
 
         is_release = other_action.current_branch.include?('release/')
--- lib/fastlane/plugin/stream_actions/actions/show_sdk_size.rb
+++ lib/fastlane/plugin/stream_actions/actions/show_sdk_size.rb
@@ -3,8 +3,9 @@ module Fastlane
     class ShowSdkSizeAction < Action
       def self.run(params)
         chat_v5_branch = 'v5'
-        is_pr_to_chat_v5 = params[:github_repo].include?('stream-chat-swift') && ENV.fetch('GITHUB_BASE_REF', nil).to_s.include?(chat_v5_branch)
-        is_push_to_chat_v5 = ENV['GITHUB_EVENT_NAME'].to_s == 'push' && other_action.current_branch == chat_v5_branch
+        is_chat_repo = params[:github_repo].include?('stream-chat-swift')
+        is_pr_to_chat_v5 = is_chat_repo && ENV.fetch('GITHUB_BASE_REF', nil).to_s.include?(chat_v5_branch)
+        is_push_to_chat_v5 = is_chat_repo && ENV['GITHUB_EVENT_NAME'].to_s == 'push' && other_action.current_branch == chat_v5_branch
         UI.important("🏗️ Base branch workaround for Chat V5") if is_pr_to_chat_v5 || is_push_to_chat_v5
 
         warning_status = '🟡' # Warning if a branch is #{max_tolerance} less performant than the benchmark
